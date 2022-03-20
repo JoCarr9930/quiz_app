d73f9c4c4c35e6b5215778a2732413683cad9702
Jose Carrizalees <josedcarrizales@gmail.com>
2022-03-20 10:29:47 -0700

    created a the base layout and pages needed for the application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Questions from "./pages/Questions";
+import Results from "./pages/Results";
+import Settings from "./pages/Settings";
+import { Box, Container, Typography } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container maxWidth="sm">
+        <Box textAlign="center" mt={5}>
+          <Switch>
+            <Route exact path="/">
+              <Typography variant="h2" fontWeight="bold">
+                Quiz App
+              </Typography>
+              <Settings />
+            </Route>
+            <Route path="/questions">
+              <Questions />
+            </Route>
+            <Route path="/results">
+              <Results />
+            </Route>
+          </Switch>
+        </Box>
+      </Container>
+    </Router>
   );
 }
 
